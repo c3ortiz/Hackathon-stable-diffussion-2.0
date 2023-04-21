1679ca3fb5ffa32e1d8806fca87dd60ca09b97ea
Cesar Ortiz <cesarortizbq@gmail.com>
2023-04-21 16:32:08 -0500

    Finished

--- main.py
+++ main.py
@@ -19,114 +19,111 @@ def index():
 
     if request.method == "POST":
 
-        if 1 == 1:
-            return redirect("/loading", code=302)
-            
 
-        sleep(5)
+        redirect("/loading", code=200)
         
-        # OPENAI_API_KEY = request.form.get("openaiapikey")
-        # STABLEDIFF_API_KEY = request.form.get("stablediffapikey")
-        # PROJECT_NAME = request.form.get("projectname")
-        # AUDIO_FILE = request.files['audioFile']
+        OPENAI_API_KEY = request.form.get("openaiapikey")
+        STABLEDIFF_API_KEY = request.form.get("stablediffapikey")
+        PROJECT_NAME = request.form.get("projectname")
+        AUDIO_FILE = request.files['audioFile']
 
-        # AUDIO_FILE.save(os.path.join(AUDIO_FOLDER, "fileFromFrontend.mp3"))
+        AUDIO_FILE.save(os.path.join(AUDIO_FOLDER, "fileFromFrontend.mp3"))
 
-        # # API WHISPER
+        # API WHISPER
 
-        # openai.api_key = OPENAI_API_KEY
-        # audio_file = open(os.path.join(AUDIO_FOLDER, "fileFromFrontend.mp3"), "rb")
-        # transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        openai.api_key = OPENAI_API_KEY
+        audio_file = open(os.path.join(AUDIO_FOLDER, "fileFromFrontend.mp3"), "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-        # promptToChatGPT = transcript.text
+        promptToChatGPT = transcript.text
 
-        # # API CHATGPT
+        # API CHATGPT
 
-        # input_prompt = "Summarize:" + promptToChatGPT
+        input_prompt = "Summarize:" + promptToChatGPT
 
-        # completionSummary = openai.ChatCompletion.create(
-        #     model = 'gpt-3.5-turbo',
-        #     messages = [
-        #         {'role': 'user', 'content': input_prompt}
-        #     ],
-        #     temperature = 0,
-        #     max_tokens = 200
-        # )
+        completionSummary = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages = [
+                {'role': 'user', 'content': input_prompt}
+            ],
+            temperature = 0,
+            max_tokens = 200
+        )
 
-        # summary = completionSummary['choices'][0]['message']['content']
+        summary = completionSummary['choices'][0]['message']['content']
 
-        # completionPrompts = openai.ChatCompletion.create(
-        #     model = 'gpt-3.5-turbo',
-        #     messages = [
-        #         {'role': 'user', 'content': "Visual description of key concepts of: " + summary}
-        #     ],
-        #     temperature = 0,
-        #     max_tokens = 200
-        # )
+        completionPrompts = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages = [
+                {'role': 'user', 'content': "Visual description of key concepts of: " + summary}
+            ],
+            temperature = 0,
+            max_tokens = 200
+        )
 
-        # promptsToStableDiffusion = completionPrompts['choices'][0]['message']['content'].split(". ")
+        promptsToStableDiffusion = completionPrompts['choices'][0]['message']['content'].split(". ")
 
-        # completionQuestions = openai.ChatCompletion.create(
-        #     model = 'gpt-3.5-turbo',
-        #     messages = [
-        #         {'role': 'user', 'content': "Questions of key concepts of: " + summary}
-        #     ],
-        #     temperature = 0,
-        #     max_tokens = 200
-        # )
+        completionQuestions = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages = [
+                {'role': 'user', 'content': "Questions of key concepts of: " + summary}
+            ],
+            temperature = 0,
+            max_tokens = 200
+        )
 
-        # questions = completionQuestions['choices'][0]['message']['content']
+        questions = completionQuestions['choices'][0]['message']['content']
 
-        # # API STABLE DIFFUSSION
+        # API STABLE DIFFUSSION
 
-        # engine_id = "stable-diffusion-v1-5"
-        # api_host = 'https://api.stability.ai'
-        # api_key = STABLEDIFF_API_KEY
+        engine_id = "stable-diffusion-v1-5"
+        api_host = 'https://api.stability.ai'
+        api_key = STABLEDIFF_API_KEY
 
-        # if api_key is None:
-        #    raise Exception("Missing Stability API key.")
+        if api_key is None:
+           raise Exception("Missing Stability API key.")
 
-        # for j in range(0, len(promptsToStableDiffusion)):
-        #     response = requests.post(
-        #         f"{api_host}/v1/generation/{engine_id}/text-to-image",
-        #         headers={
-        #             "Content-Type": "application/json",
-        #             "Accept": "application/json",
-        #             "Authorization": f"Bearer {api_key}"
-        #         },
-        #         json={
-        #             "text_prompts": [
-        #                 {   
-        #                     "text": promptsToStableDiffusion[j]
-        #                 }
-        #             ],
-        #         "cfg_scale": 7,
-        #         "clip_guidance_preset": "FAST_BLUE",
-        #         "height": 250,
-        #         "width": 250,
-        #         "samples": 1,
-        #         "steps": 30,
-        #         },
-        #     )
+        for j in range(0, len(promptsToStableDiffusion)):
+            response = requests.post(
+                f"{api_host}/v1/generation/{engine_id}/text-to-image",
+                headers={
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": f"Bearer {api_key}"
+                },
+                json={
+                    "text_prompts": [
+                        {   
+                            "text": promptsToStableDiffusion[j]
+                        }
+                    ],
+                "cfg_scale": 7,
+                "clip_guidance_preset": "FAST_BLUE",
+                "height": 512,
+                "width": 512,
+                "samples": 1,
+                "steps": 30,
+                },
+            )
 
-        #     if response.status_code != 200:
-        #         raise Exception("Non-200 response: " + str(response.text))
+            if response.status_code != 200:
+                raise Exception("Non-200 response: " + str(response.text))
 
-        #     data = response.json()
+            data = response.json()
 
-        #     for i, image in enumerate(data["artifacts"]):
-        #         with open(f"static/images/txt2img_{j}.png", "wb") as f:
-        #             f.write(base64.b64decode(image["base64"]))
+            for i, image in enumerate(data["artifacts"]):
+                with open(f"static/images/txt2img_{j}.png", "wb") as f:
+                    f.write(base64.b64decode(image["base64"]))
 
-        # image_urls = []
+        image_urls = []
 
-        # for i in range(0, len(promptsToStableDiffusion)):
-        #     image_urls.append(os.path.join(PEOPLE_FOLDER, f'txt2img_{i}.png'))
+        for i in range(0, len(promptsToStableDiffusion)):
+            image_urls.append(os.path.join(PEOPLE_FOLDER, f'txt2img_{i}.png'))
 
-        # session['summary'] = summary
-        # session['questions'] = questions
-        # session['images'] = image_urls
-        # session['projectName'] = PROJECT_NAME
+        session['summary'] = summary
+        session['questions'] = questions
+        session['images'] = image_urls
+        session['projectName'] = PROJECT_NAME
 
         return redirect("/response", code=302)
     
